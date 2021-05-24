7c888bd191ed32321de87c8d7f6268a4eef32e5a
Thang Do <thang.do@paradox.ai>
2021-05-24 13:53:46 +0700

    feat: added ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -42,9 +42,19 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_staff)
 
     def test_tag_str(self):
+        """ Test the tag string representation """
         tag = models.Tag.objects.create(
             user=sample_user(),
             name='Vegan',
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """ Test the ingredient string representation """
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Herb',
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
